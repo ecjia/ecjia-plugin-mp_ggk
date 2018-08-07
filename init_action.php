7d4a1425dfd30a75bc208fad8554ea8a370c3c9a
张春晨 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-07 16:32:39 +0800

    更新活动未开启的提示

--- init_action.php
+++ init_action.php
@@ -85,13 +85,18 @@ class mp_ggk_init_action implements PluginPageInterface
         $openid = trim($_GET['openid']);
         $uuid = trim($_GET['uuid']);
 
+        $code = 'wechat_guaguale';
         $platform_account = new Ecjia\App\Platform\Frameworks\Platform\Account($uuid);
 
         $wechat_id = $platform_account->getAccountID();
         $store_id = $platform_account->getStoreId();
 
-        $code = 'wechat_guaguale';
-        $MarketActivity = new Ecjia\App\Market\Prize\MarketActivity($code, $store_id, $wechat_id);
+        try {
+            $MarketActivity = new Ecjia\App\Market\Prize\MarketActivity($code, $store_id, $wechat_id);
+        } catch (Ecjia\App\Market\Exceptions\ActivityException $e) {
+            return ecjia_front::$controller->showmessage($e->getMessage(), ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_ERROR);
+        }
+
         $starttime = $MarketActivity->getActivityStartTime();
         $endtime = $MarketActivity->getActivityEndTime();
         $time = RC_Time::gmtime();
@@ -142,7 +147,13 @@ class mp_ggk_init_action implements PluginPageInterface
         $store_id = $platform_account->getStoreId();
 
         $code = 'wechat_guaguale';
-        $MarketActivity = new Ecjia\App\Market\Prize\MarketActivity($code, $store_id, $wechat_id);
+
+        try {
+            $MarketActivity = new Ecjia\App\Market\Prize\MarketActivity($code, $store_id, $wechat_id);
+        } catch (Ecjia\App\Market\Exceptions\ActivityException $e) {
+            return ecjia_front::$controller->showmessage($e->getMessage(), ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_ERROR);
+        }
+
         $starttime = $MarketActivity->getActivityStartTime();
         $endtime = $MarketActivity->getActivityEndTime();
         $time = RC_Time::gmtime();
