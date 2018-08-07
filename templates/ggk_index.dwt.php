123f7963150b3cb627398d6e39bf11f279d9c3c9
张春晨 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-07 15:52:56 +0800

    样式调整

--- templates/ggk_index.dwt.php
+++ templates/ggk_index.dwt.php
@@ -30,16 +30,10 @@
 	                    {if $prize}
 	                    <div class="Detail">
 	                        <!-- {foreach from=$prize item=val }-->
-	                        <p>
-	                            {if $val.prize_level eq '0'} 特等奖：
-	                            {elseif $val.prize_level eq '1'} 一等奖：
-	                            {elseif $val.prize_level eq '2'} 二等奖：
-	                            {elseif $val.prize_level eq '3'} 三等奖：
-	                            {elseif $val.prize_level eq '4'} 四等奖：
-	                            {elseif $val.prize_level eq '5'} 五等奖：
-	                            {/if}
-	                            {$val.prize_name}{if $val.prize_type eq '1' || $val.prize_type eq '3'}{$val.prize_value}（剩余奖品数量：{$val.prize_number}）{/if}
-	                        </p>
+                            <p>
+                                {if $val.prize_level eq '0'} 特等奖： {elseif $val.prize_level eq '1'} 一等奖： {elseif $val.prize_level eq '2'} 二等奖： {elseif $val.prize_level
+                                eq '3'} 三等奖： {elseif $val.prize_level eq '4'} 四等奖： {elseif $val.prize_level eq '5'} 五等奖： {/if} {$val.prize_name}{$val.prize_value}（剩余奖品数量：{$val.prize_number}）
+                            </p>
 	                        <!-- {/foreach} -->
 	                    </div>
 	                    {else}
